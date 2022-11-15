06145330d898ec870f11353f771ff66a78864a9d
RevEng-AI <root+github@reveng.ai>
2022-11-15 13:55:17 +0000

    Add default ption to binary, bump to 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
         name="reait",
-        version="0.0.4",
+        version="0.0.5",
         scripts=['reait'],
         long_description=long_description,
         long_description_content_type="text/markdown",
