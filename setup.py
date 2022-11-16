1733dcc4e106aa24ef3369824da4556840b5cef9
RevEng-AI <root+github@reveng.ai>
2022-11-16 14:23:47 +0000

    Bump to version 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
         name="reait",
-        version="0.0.6",
+        version="0.0.7",
         scripts=['reait'],
         long_description=long_description,
         long_description_content_type="text/markdown",
