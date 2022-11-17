167d2564860b8acfc30b0230d971480b2a00f158
RevEng-AI <root+github@reveng.ai>
2022-11-17 15:20:45 +0000

    Fix sklearn dependency package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
         name="reait",
-        version="0.0.10",
+        version="0.0.11",
         scripts=['reait'],
         long_description=long_description,
         long_description_content_type="text/markdown",
@@ -17,7 +17,7 @@ setuptools.setup(
             "Operating System :: OS Independent"
             ],
         install_requires=[
-                'tqdm', 'argparse', 'requests', 'rich', 'tomli', 'sklearn', 'pandas', 'numpy'
+                'tqdm', 'argparse', 'requests', 'rich', 'tomli', 'scikit-learn', 'pandas', 'numpy'
             ]
         )
 
