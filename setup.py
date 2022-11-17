ea33d567e252767f86c440803b500e9c0d5fed2e
RevEng-AI <root+github@reveng.ai>
2022-11-17 15:43:49 +0000

    Report vaddr, not iloc

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
         name="reait",
-        version="0.0.11",
+        version="0.0.12",
         scripts=['reait'],
         long_description=long_description,
         long_description_content_type="text/markdown",
