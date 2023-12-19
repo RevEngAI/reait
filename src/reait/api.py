a6ebc745e83372beaf9702489dfcd0931693e907
vgoat21 <joshua.villaltaesp@gmail.com>
2023-12-19 12:06:13 +0000

    fixing analyse

--- src/reait/api.py
+++ src/reait/api.py
@@ -147,7 +147,7 @@ def RE_analyse(fpath: str, model: str = None, isa_options: str = None, platform_
 
     if res.status_code == 400:
         print(res)
-        if 'error' in response.keys():
+        if 'error' in res.keys():
             print(
                 f"[-] Error analysing {fpath} - {response['error']}.")
             return res
