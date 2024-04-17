2302b990016abff1d067ae44c94e4d633ea99754
Yannick Tournier <yannick@reveng.ai>
2024-04-17 19:20:10 +0200

    Add choices for scope option

--- src/reait/main.py
+++ src/reait/main.py
@@ -252,7 +252,7 @@ def main() -> None:
                         help="Enable dynamic execution in sandbox during analysis. Analysis will include any auto unpacked malware samples")
     parser.add_argument("--cmd-line-args", default="",
                         help="Command line arguments to pass when running binary sample in the sandbox. Only used when run with --dynamic-execution")
-    parser.add_argument("--scope", default="private",
+    parser.add_argument("--scope", default="private", choices=["public", "private"],
                         help="Override analysis visibility (scope). Valid values are 'public' or 'private'[DEFAULT]")
     parser.add_argument("--tags", default=None, type=str,
                         help="Assign tags to an analysis. Valid responses are tag1,tag2,tag3..")
